73ac03a76e65bb6cf7a869062ee7fcffabe0a8c5
Radosław Gryta <radek.gryta@gmail.com>
2026-01-10 11:12:49 +0100

    Add dependency update checks with ben-manes versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,16 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.native.cocoapods) apply false
     alias(libs.plugins.vanniktech) apply false
+    alias(libs.plugins.versions)
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any {
+            candidate.version.uppercase().contains(it)
+        }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(candidate.version)
+        isStable.not()
+    }
 }
